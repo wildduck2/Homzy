4ec5971af3b6b44f5a89b1a706b2bb3529afaeef
ahmed <wezonaser50@gmail.com>
2023-09-11 16:21:45 +0300

    fixing vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
     plugins: [react()],
-    //    base: '/Homzy/',
-=======
-  plugins: [react()],
->>>>>>> f933ec859f53d0ee9db29cbbe96379975b308249
+    base: '/Homzy/',
 })
