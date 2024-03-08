13e117514a05c90c224492cd4d4c71666571e83c
thecoder512 <wezonaser50@gmail.com>
2024-03-08 19:20:43 +0200

    build: fixin route issues

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
-import './scss/styles.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import "./scss/styles.scss";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <React.StrictMode>
-        <BrowserRouter basename='/Homzy/'>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
